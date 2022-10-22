c91a9b5781acf2a73730c7573d504cd5421f72a3
Zaki <zakibonnor2001@gmail.com>
2022-10-22 18:29:37 +0100

    Added The 4 Arithmetics

--- BasicPythonCalculator/calculator.py
+++ BasicPythonCalculator/calculator.py
@@ -1,4 +1,33 @@
-num1=input("enter the 1st Number")
-num2=input("enter the 2nd Number")
-s=float(num1)+float(num2)
-print(s)
+from ast import operator
+
+
+print("Select An Operation To Perform")
+print("1-Addition")
+print("2-Substraction")
+print("3-Multipication")
+print("4-Division")
+
+operation=input()
+
+if operation == "1":
+    num1=input("enter the 1st Number")
+    num2=input("enter the 2nd Number")
+    print("The Sum is"+str(int(num1)+int(num2)))
+
+elif operation == "2":
+    num1=input("enter the 1st Number")
+    num2=input("enter the 2nd Number")
+    print("The Sum is"+str(int(num1)-int(num2)))
+
+elif operation == "3":
+    num1=input("enter the 1st Number")
+    num2=input("enter the 2nd Number")
+    print("The Sum is"+str(int(num1)*int(num2)))
+elif operation == "4":
+    num1=input("enter the 1st Number")
+    num2=input("enter the 2nd Number")
+    print("The Sum is"+str(int(num1)/int(num2)))
+else: print("Invalid Entry")
+
+
+
